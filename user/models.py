b408cdda04a967c58088fda2b6df3c07ff090660
Action2theFuture <dnstks0204@google.com>
2021-05-10 17:11:18 +0900

    User Model 작성

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    email        = models.EmailField(max_length=128, unique=True)
+    name         = models.CharField(max_length =32)
+    password     = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length=32, unique=True)
+    birthday     = models.CharField(max_length=32)
+    create_at    = models.DateTimeField(auto_now_add=True)
+    update_at    = models.DateTimeField(auto_now =True)
+
+    def Meta:
+        db_table = "users"
