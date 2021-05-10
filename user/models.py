3c655fc766427a0b47f72368b1e0849c163ac87d
Action2theFuture <dnstks0204@google.com>
2021-05-10 17:17:46 +0900

    first_name, last_name 필드 생성, name 필드 삭제

--- user/models.py
+++ user/models.py
@@ -3,12 +3,13 @@ from django.db import models
 # Create your models here.
 class Users(models.Model):
     email        = models.EmailField(max_length=128, unique=True)
-    name         = models.CharField(max_length =32)
+    first_name   = models.CharField(max_length=32)
+    last_name    = models.CharField(max_length=32)
     password     = models.CharField(max_length=128)
     phone_number = models.CharField(max_length=32, unique=True)
     birthday     = models.CharField(max_length=32)
     create_at    = models.DateTimeField(auto_now_add=True)
-    update_at    = models.DateTimeField(auto_now =True)
+    update_at    = models.DateTimeField(auto_now=True)
 
     def Meta:
         db_table = "users"
