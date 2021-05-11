32fb61bde4dbc2dc5e889a57142a8343bf55f7f8
Action2theFuture <dnstks0204@google.com>
2021-05-11 18:47:38 +0900

    user, order model 작성

--- user/models.py
+++ user/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Users(models.Model):
+class User(models.Model):
     email        = models.EmailField(max_length=128, unique=True)
     first_name   = models.CharField(max_length=32)
     last_name    = models.CharField(max_length=32)
@@ -11,5 +11,31 @@ class Users(models.Model):
     create_at    = models.DateTimeField(auto_now_add=True)
     update_at    = models.DateTimeField(auto_now=True)
 
-    def Meta:
+    class Meta:
         db_table = "users"
+
+class Order(models.Model):
+    first_name   = models.CharField(max_length=32)
+    last_name    = models.CharField(max_length=32)
+    address      = models.CharField(max_length=128)
+    sub_address  = models.CharField(max_length=128)
+    user         = models.ForeignKey("User", on_delete=models.CASCADE)
+    status       = models.ForeignKey("OrderStatus", on_delete=models.CASCADE)
+    order_list   = models.ManyToManyField(Product, through="OrderList")
+
+    class Meta:
+        db_table = "orders"
+
+class OrderStatus(models.Model):
+    status       = models.CharField(max_length=32)
+
+    class Meta:
+        db_table = "order_status"
+
+class OrderList(models.Model):
+    quantity     = models.IntegerField(default=1)
+    order        = models.ForeignKey("Order", on_delete=models.CASCADE)
+    product      = models.ForeignKey(Product, on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = "order_lists"
