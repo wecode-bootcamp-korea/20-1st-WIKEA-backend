a8c2dc954bfb778dda9d6c2037e42655053d88f6
Action2theFuture <dnstks0204@google.com>
2021-05-11 18:50:40 +0900

    wishlist 작성

--- user/models.py
+++ user/models.py
@@ -10,6 +10,7 @@ class User(models.Model):
     birthday     = models.CharField(max_length=32)
     create_at    = models.DateTimeField(auto_now_add=True)
     update_at    = models.DateTimeField(auto_now=True)
+    wish_list    = models.ManyToManyField(Product, through="WishList")
 
     class Meta:
         db_table = "users"
@@ -36,6 +37,13 @@ class OrderList(models.Model):
     quantity     = models.IntegerField(default=1)
     order        = models.ForeignKey("Order", on_delete=models.CASCADE)
     product      = models.ForeignKey(Product, on_delete=models.CASCADE)
-    
+
     class Meta:
         db_table = "order_lists"
+
+class WishList(models.Model):
+    user         = models.ForeignKey("User", on_delete=models.CASCADE)
+    product      = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "wish_list"
