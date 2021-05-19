76f8634bc43cbfdc44c76ed0e941cddf2f5d15e7
Action2theFuture <dnstks0204@google.com>
2021-05-19 11:44:20 +0900

    commit

--- product/views.py
+++ product/views.py
@@ -9,9 +9,9 @@ from product.sub_product_queryset import get_queryset
 
 class SearchView(View):
     def search(self, request):
-        data = json.loads(request.body)
-        
-        products = [product.values() for product in Product.object.filter(korean_name__contain = data['korean_name'])
+        search = request.GET.get('search')
+        if search:
+            products = [product.values() for product in Product.object.filter(korean_name__contain = search)
 
         return JsonResponse({'product':products}, status=200)
 
