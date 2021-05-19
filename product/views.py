edf8a5e1698fc0bc3bd390a89373c8f2192a5c00
Action2theFuture <dnstks0204@google.com>
2021-05-19 12:19:47 +0900

    Add : Q 조건 추가

--- product/views.py
+++ product/views.py
@@ -11,12 +11,14 @@ from product.sub_product_queryset import get_queryset
 class SearchView(View):
     def search(self, request):
         search = request.GET.get('q')
-        
+
         if search:
             products = [product.values() for product in Product.object.filter(
                 Q(korean_name__contain = search) & Q(english_name_contain = search) 
                 &Q(sub_category__korean_name__contain = search) & Q(sub_category__english_name__contain = search) 
-                &Q(category__korean_name__contain = search) & Q(category__korean_name__contatin = search)).distinct()]
+                &Q(category__korean_name__contain = search) & Q(category__korean_name__contatin = search)
+                &Q(series__korean_name__contain = search) & Q(series__english_name_contain = search)
+                &Q(color__korean_name__contain = search) & Q(color__english_name_contain = search)).distinct()]
 
             result = [{
                         'korean_name'       : product.korean_name,
