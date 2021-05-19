99f7421bdba797480c801c3967ff6cf668bc7a56
Action2theFuture <dnstks0204@google.com>
2021-05-19 12:43:26 +0900

    Add : 검색어에 해당되지 않는 상품은 404

--- product/views.py
+++ product/views.py
@@ -10,27 +10,31 @@ from product.sub_product_queryset import get_queryset
 
 class SearchView(View):
     def search(self, request):
-        search = request.GET.get('q')
+        try:
+            search = request.GET.get('q')
 
-        if search:
-            products = [product.values() for product in Product.object.filter(
-                Q(korean_name__contain = search) & Q(english_name_contain = search) 
-                &Q(sub_category__korean_name__contain = search) & Q(sub_category__english_name__contain = search) 
-                &Q(category__korean_name__contain = search) & Q(category__korean_name__contatin = search)
-                &Q(series__korean_name__contain = search) & Q(series__english_name_contain = search)
-                &Q(color__korean_name__contain = search) & Q(color__english_name_contain = search)).distinct()]
+            if search:
+                products = [product.values() for product in Product.object.filter(
+                    Q(korean_name__contain = search) & Q(english_name_contain = search) 
+                    &Q(sub_category__korean_name__contain = search) & Q(sub_category__english_name__contain = search) 
+                    &Q(category__korean_name__contain = search) & Q(category__korean_name__contatin = search)
+                    &Q(series__korean_name__contain = search) & Q(series__english_name_contain = search)
+                    &Q(color__korean_name__contain = search) & Q(color__english_name_contain = search)).distinct()]
 
-            result = [{
-                        'korean_name'       : product.korean_name,
-                        'english_name'      : product.english_name,
-                        'price'             : product.price,
-                        'special_price'     : product.special_price,
-                        'is_new'            : product.is_new,
-                        'color_list'        : [color.name for color in product.color.all()],
-                        'image'             : [image.url for image in product.image.all()],
-                        'series'            : series,
-                    } for product in products]
+                result = [{
+                            'korean_name'       : product.korean_name,
+                            'english_name'      : product.english_name,
+                            'price'             : product.price,
+                            'special_price'     : product.special_price,
+                            'is_new'            : product.is_new,
+                            'color_list'        : [color.name for color in product.color.all()],
+                            'image'             : [image.url for image in product.image.all()],
+                            'series'            : series,
+                        } for product in products]
 
-        return JsonResponse({'search':result}, status=200)
+            return JsonResponse({'search':result}, status=200)
+            
+        except Product.DoesNotExist:
+            return JsonResponse({'search':'non-existent product'}, status=404)
 
 
