8666b545c6034d6d79e49a59c3323431822648cb
Action2theFuture <dnstks0204@google.com>
2021-05-20 10:31:06 +0900

    Add : q값이 None인 상황 추가

--- product/views.py
+++ product/views.py
@@ -19,18 +19,17 @@ class SearchView(View):
                     |Q(color__korean_name__contains = search) | Q(color__english_name__contains = search)
                     |Q(sub_category__category__korean_name__contains = search) | Q(sub_category__category__english_name__contains = search))
 
-                result = [{
-                            'korean_name'       : product.korean_name,
-                            'english_name'      : product.english_name,
-                            'price'             : product.price,
-                            'special_price'     : product.special_price,
-                            'is_new'            : product.is_new,
-                            'color_list'        : [color.korean_name for color in product.color.all()],
-                            'image'             : [image.url for image in product.image.all()],
-                        } for product in products]
+            result = [{
+                        'korean_name'       : product.korean_name,
+                        'english_name'      : product.english_name,
+                        'price'             : product.price,
+                        'special_price'     : product.special_price,
+                        'is_new'            : product.is_new,
+                        'color_list'        : [color.korean_name for color in product.color.all()],
+                        'image'             : [image.url for image in product.image.all()],
+                    } for product in products]
 
-                return JsonResponse({'search':result}, status=200)
-            
+            return JsonResponse({'search':result}, status=200)
         except Product.DoesNotExist:
             return JsonResponse({'search':'non-existent product'}, status=404)
 
